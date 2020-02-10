1906e1578dbf6575370267eb381de3c01ac49776
Maxim Petrashev <mpetrashev@mail.ru>
2020-02-10 22:41:23 +0000

    Fixed test for D's env

--- test/main/python/mll/Tutorial_test.py
+++ test/main/python/mll/Tutorial_test.py
@@ -8,7 +8,7 @@ class Tutorial_test(unittest.TestCase):
     def test_prediction(self):
         t = Tutorial()
         prediction = t.predict()
-        self.assertAlmostEqual(0.60139096, prediction[0][0][0])
+        self.assertAlmostEqual(0.60139096, prediction[0][0][0], places=6)
 
 
 if __name__ == '__main__':
