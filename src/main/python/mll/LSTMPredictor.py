f218b548441ec8eecc43c70d04c3df31c13e8025
Maxim Petrashev <mpetrashev@mail.ru>
2020-03-15 18:55:44 +0000

    Small refactoring

--- src/main/python/mll/LSTMPredictor.py
+++ src/main/python/mll/LSTMPredictor.py
@@ -7,23 +7,6 @@ import tensorflow as tf
 from utils import lazy_property
 
 
-# todo refactor this method
-def multivariate_data(dataset, target, start_index, end_index, history_size, target_size, step):
-    data = []
-    labels = []
-
-    start_index = start_index + history_size
-    if end_index is None:
-        end_index = len(dataset) - target_size
-
-    for i in range(start_index, end_index):
-        indices = range(i - history_size, i, step)
-        data.append(dataset[indices])
-        labels.append(target[i:i + target_size])
-
-    return np.array(data), np.array(labels)
-
-
 class LSTMPredictor:
     def __init__(self, time_series: pd.DataFrame, feature_to_predict: Union[str, int], train_split: int
                  , past_history: int, future_target: int, step: int = 1, batch_size: int = 256,
@@ -46,17 +29,43 @@ class LSTMPredictor:
         train_ds = dataset[:self.train_split]
         return (dataset - train_ds.mean(axis=0)) / train_ds.std(axis=0)
 
+    # todo refactor this method
     @lazy_property
-    def train_data_multi(self):
-        self.x_train_multi, y_train_multi = multivariate_data(self.dataset, self.dataset[:, self.feature_to_predict], 0
-                            , self.train_split, self.past_history, self.future_target, self.step)
+    def multivariate_data(self):
+        """
+        :param dataset: Timeseries of explanatory variables.
+        :param target: Timeseries of explained variable.
+        :return: Tuple with:
+            - Cube of explanatory variables sets per each step and
+            - Set of explained variable sets per each step.
+        """
+        dataset = self.dataset
+        target_size = self.future_target
+        history_size = self.past_history
+        target = self.dataset[:, self.feature_to_predict]
+        step = self.step
+        data = []
+        labels = []
+
+        for i in range(history_size, len(dataset) - target_size):
+            indices = range(i - history_size, i, step)
+            data.append(dataset[indices])
+            labels.append(target[i:i + target_size])
+
+        return np.array(data), np.array(labels)
+
+    @lazy_property
+    def training_dataset(self):
+        x, y = self.multivariate_data
+        boundary = self.train_split - self.past_history
+        self.x_train_multi, y_train_multi = x[0:boundary], y[0:boundary]
         train_data_multi = tf.data.Dataset.from_tensor_slices((self.x_train_multi, y_train_multi))
         return train_data_multi.cache().shuffle(self.buffer_size).batch(self.batch_size).repeat()
 
     @lazy_property
     def validation_dataset(self):
-        x_val_multi, y_val_multi = multivariate_data(self.dataset, self.dataset[:, self.feature_to_predict]
-                            , self.train_split, None, self.past_history, self.future_target, self.step)
+        x, y = self.multivariate_data
+        x_val_multi, y_val_multi = x[self.train_split:], y[self.train_split:]
         validation_dataset = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
         validation_dataset = validation_dataset.batch(self.batch_size).repeat()
         return validation_dataset
@@ -70,8 +79,8 @@ class LSTMPredictor:
         multi_step_model.add(tf.keras.layers.Dense(self.future_target))
 
         multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mae')
-        self.multi_step_history = multi_step_model.fit(self.train_data_multi, epochs=self.epochs,
-                                                  steps_per_epoch=self.evaluation_interval,
-                                                  validation_data=validation_dataset,
-                                                  validation_steps=50)
+        self.multi_step_history = multi_step_model.fit(self.training_dataset, epochs=self.epochs,
+                                                       steps_per_epoch=self.evaluation_interval,
+                                                       validation_data=validation_dataset,
+                                                       validation_steps=50)
         return multi_step_model
