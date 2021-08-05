7d7fcf00c31f627d301dbaae61e6889e660c7c8e
Maxim Petrashev <mpetrashev@mail.ru>
2021-08-05 18:04:23 +0100

    Added output_layer_bias_shift. Would need to configure later to speedup training (and take initial values from previous training)

--- src/main/python/dl/TorchApproximator.py
+++ src/main/python/dl/TorchApproximator.py
@@ -1,6 +1,7 @@
 import logging
 import math
 import sys
+from typing import Callable
 
 import torch
 import torch.nn.functional as F
@@ -11,6 +12,7 @@ import gc
 from utils import vrange
 
 logger = logging.getLogger(__file__)
+# todo change data.cpu().numpy().item() on item()
 
 
 def DataLoader(x, y, batch_size=None):
@@ -27,15 +29,27 @@ def DataLoader(x, y, batch_size=None):
 
 
 class Net(torch.nn.Module):
-    def __init__(self, n_feature, n_hidden, n_layers, n_output):
+    def __init__(self, n_features: int, n_hiddens:int, n_layers:int, n_outputs: int, output_layer_bias_shift: int = None):
+        """
+        :param n_features: 
+        :param n_hiddens: 
+        :param n_layers: 
+        :param n_outputs: 
+        :param output_layer_bias_shift: Controls initial bias for output layer. User can set it to the mean of training
+        PVs to speed up the convergence.
+        """
         super(Net, self).__init__()
 
-        self.n_hidden = n_hidden
+        self.n_hidden = n_hiddens
         self.n_layers = n_layers
 
-        self.linears = torch.nn.ModuleList([torch.nn.Linear(n_feature, n_hidden)])
-        self.linears.extend([torch.nn.Linear(n_hidden, n_hidden) for i in range(1, n_layers)])
-        self.linears.append(torch.nn.Linear(n_hidden, n_output))
+        self.linears = torch.nn.ModuleList([torch.nn.Linear(n_features, n_hiddens)])
+        self.linears.extend([torch.nn.Linear(n_hiddens, n_hiddens) for i in range(1, n_layers)])
+        self.linears.append(torch.nn.Linear(n_hiddens, n_outputs))
+
+        if output_layer_bias_shift:
+            for layer in self.linears[-1:]:
+                layer.bias.data = torch.add(layer.bias.data, output_layer_bias_shift)
 
     def forward(self, x):
         for lin in self.linears[:-1]:
@@ -102,8 +116,8 @@ def run_batch(net, x_, y_, x_test, y_test, loss_func, optimizer, test_batch_size
 
 
 # todo move device to object var
-def fit_net(net: Net, n_epochs: int, x: torch.tensor, y: torch.tensor, pct_test: float, pct_validation: float, lr=0.01
-            , batch_size: int = None, shuffle=False, device: str='cpu'):
+def fit_net(net: Net, n_epochs: int, x: torch.tensor, y: torch.tensor, pct_test: float, pct_validation: float,
+            loss_func : Callable, lr=0.01, batch_size: int = None, shuffle=False, device: str='cpu'):
 
     n = len( y ) #.size()[0]
     n_train = int(np.round(n * (1 - pct_test - pct_validation))) # todo rename
@@ -118,7 +132,6 @@ def fit_net(net: Net, n_epochs: int, x: torch.tensor, y: torch.tensor, pct_test:
 
 
     optimizer = torch.optim.Adam(net.parameters(), lr=lr)
-    loss_func = torch.nn.MSELoss()  # Loss/cost function
     try:
         net.to(device)  # todo do we need it?
 
@@ -157,8 +170,9 @@ def fit_net(net: Net, n_epochs: int, x: torch.tensor, y: torch.tensor, pct_test:
 
 class TorchApproximator:
 
-    def __init__(self, seed: int = 314, device: str = None) -> None:
+    def __init__(self, seed: int = 314, device: str = None, loss_func: Callable = None) -> None:
         super().__init__()
+        self.loss_func = loss_func if loss_func else torch.nn.MSELoss()  # Loss/cost function
         if device is None:
             if torch.cuda.is_available():
                 device = 'cuda'
@@ -172,7 +186,7 @@ class TorchApproximator:
 
     def train(self, states, pvs, n_epochs=6000, pct_test=0.2  # Portion for test set
               , pct_validation=0.1  # Portion for validation set
-              , n_hidden: int = 100, n_layers: int = 4, lr=0.01, batch_size: int = None):
+              , n_hiddens: int = 100, n_layers: int = 4, lr=0.01, batch_size: int = None):
         self.samples_t = torch.from_numpy(states).float() #todo rename samples and values to x and y
         self.values_t = torch.from_numpy(pvs).float().unsqueeze(dim=1)
         self.pvs = pvs
@@ -181,24 +195,24 @@ class TorchApproximator:
 
         self.pct_validation = pct_validation
         n_features = states.shape[1]
-        net = Net(n_feature=n_features, n_hidden=n_hidden, n_layers=n_layers, n_output=1)  # define the network
-        best_loss_test, checkpoint, df = fit_net(net, n_epochs, self.samples_t, self.values_t, pct_test, pct_validation, lr=lr
-                                     , device=self.device, batch_size=batch_size)
+        net = Net(n_features=n_features, n_hiddens=n_hiddens, n_layers=n_layers, n_outputs=1)  # define the network
+        best_loss_test, checkpoint, df = fit_net(net, n_epochs, self.samples_t, self.values_t, pct_test, pct_validation,
+                                    self.loss_func, lr=lr, device=self.device, batch_size=batch_size)
         checkpoint['n_features'] = n_features
         return checkpoint, df, best_loss_test
 
     def load_model(self, checkpoint):
-        model = Net(n_feature=checkpoint['n_features'],
-                    n_hidden=checkpoint['n_hidden'],
+        model = Net(n_features=checkpoint['n_features'],
+                    n_hiddens=checkpoint['n_hidden'],
                     n_layers=checkpoint['n_layers'],
-                    n_output=1)
+                    n_outputs=1)
         model.load_state_dict(checkpoint['model_state_dict'])
         model.eval()
         model.to(self.device)
         return model
 
     def validation_set(self, model):
-        n = len( self.values_t ) #.size()[0]
+        n = len(self.values_t)
         ind_validation = int(np.round(n * (1 - self.pct_validation)))
         samples_validation = self.samples_t[ind_validation:].to(self.device)
         approximation = model(samples_validation).flatten().data.cpu().numpy()  # approximated PVs
